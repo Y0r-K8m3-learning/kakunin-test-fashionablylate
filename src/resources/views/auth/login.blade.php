240bf5acd9fc9c5d2be701b66a56a89f90c8515b
hoge git <git.test@gmail.com>
2024-06-10 19:17:58 +0900

    fix:env

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
     @csrf
     <div class="form__group">
       <div class="form__group-title">
-        <span class="form__label--item">メールアドレス</span>
+        <span class="form__label--item">メーssルアドレス</span>
       </div>
       <div class="form__group-content">
         <div class="form__input--text">
